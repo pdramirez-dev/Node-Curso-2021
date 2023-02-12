5c21ddc6d4b7e40e8a1507cb143a1c7ca7bb7581
Pablo Enrique Diaz Ramírez <54461878+pdramirez-dev@users.noreply.github.com>
2023-02-11 22:53:42 -0500

    Delete 06-web-server directory

--- 06-web-server/app-old.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-
-http.createServer((req, res) => {
-    res.write("Hola Mundo");
-    res.end();
-  })
-  .listen(3000);
-
-console.log("Escuchando en el puerto", 3000);
